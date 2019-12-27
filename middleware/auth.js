8480bb1071e0e7f04ff0d29edb06b760cffcb971
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-12-27 15:18:52 +0100

    made a small change

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = (req, res, next) => {
-  const token = req.header("auth-token");
+  const token = req.header("authToken");
 
   if (!token) {
     return res.status(401).send("Access denied the token was not provided");
