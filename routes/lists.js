5a75e96069ca536f0ed639ecc0aec7b2e4b299ec
AlexJ2005 <alexanderjernstrom@gmail.com>
2020-03-07 10:52:21 +0100

    fixed the delete route

--- routes/lists.js
+++ routes/lists.js
@@ -21,12 +21,16 @@ router.post("/create", authenticate, async (req, res) => {
     .catch(err => res.send(err));
 });
 
-router.delete("/delete", authenticate, async (req, res) => {
-  const list = await List.findById(req.body_id);
-  list.remove((err, list) => {
-    if (err) return res.status(404).send("List could not be deleted");
-    res.status(200).send(`${list.name} was succesfully deleted`);
-  });
+router.delete("/delete/:id", authenticate, async (req, res) => {
+  const list = await List.findById(req.params.id);
+  if (list) {
+    list.remove((err, list) => {
+      if (err) return res.status(404).send("List could not be deleted");
+      res.status(200).send(`${list.name} was succesfully deleted`);
+    });
+  } else {
+    res.send("List was not found");
+  }
 });
 
 router.patch("/edit", authenticate, async (req, res) => {
