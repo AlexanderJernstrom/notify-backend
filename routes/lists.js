a776d23d5b63076ffe7af894b5842dbac36e3730
AlexJ2005 <alexanderjernstrom@gmail.com>
2020-03-07 10:39:36 +0100

    delete list route added

--- routes/lists.js
+++ routes/lists.js
@@ -21,6 +21,14 @@ router.post("/create", authenticate, async (req, res) => {
     .catch(err => res.send(err));
 });
 
+router.delete("/delete", authenticate, async (req, res) => {
+  const list = await List.findById(req.body_id);
+  list.remove((err, list) => {
+    if (err) return res.status(404).send("List could not be deleted");
+    res.status(200).send(`${list.name} was succesfully deleted`);
+  });
+});
+
 router.patch("/edit", authenticate, async (req, res) => {
   let list = await List.findById(req.body._id);
   const newList = req.body.list;
