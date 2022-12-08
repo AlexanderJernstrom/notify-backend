aef267bddc369f6bd33d5a996113c396578dd003
Alexander Jernström <46424392+AlexanderJernstrom@users.noreply.github.com>
2022-12-08 20:40:59 +0100

    Initial commit

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use("/api/user", userRoute);
 app.use("/api/notes", notesRoute);
 app.use("/api/lists", listRoute);
 
+console.log(process.env.MONGO_URI)
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Database connected"))
